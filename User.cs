1ba3abb6479e2d3c4022b2c547dd7c68df12281e
Evgenii Kucheriavyi <e.kucheriavii@gmail.com>
2019-10-20 21:40:21 +0300

    Added User class

--- User.cs
+++ User.cs
@@ -8,10 +8,59 @@ namespace Api
 		private string login;
 		private string password;
 
-		public User(string login, string password)
+		public User(string login, string password, List<User> users)
 		{
-			
+			if(CheckLogin(login, users))
+			{
+				this.login = login;
+				this.password = password;
+				this.key = GenerateKey(users);
+			}
 		}
+
+		private bool CheckLogin(string login, List<User> users)
+		{
+			result = true;
+
+			foreach(User user in users)
+			{
+				if(user.login == login)
+				{
+					result = false;
+					break;
+				}
+			}
+
+			return result;
+		}
+
+		private string GenerateKey(List<User> users)
+		{
+			Random rand = new Random();
+
+			int key = rand.Next(100, 10000);
+
+			string xKey = key.ToString(16);
+
+			bool free = true;
+
+			foreach(User user in users)
+			{
+				if(user.Key == xKey)
+				{
+					free = false;
+					break;
+				}
+			}
+
+			if(!free)
+			{
+				xKey = GenerateKey(users);
+			}
+
+			return xkey;
+		}
+
 
 		public string Key 
 		{ 
@@ -21,12 +70,20 @@ namespace Api
 			}
 		}
 
+		public string Login
+		{ 
+			get
+			{
+				return this.login;
+			}
+		}
 
-		private string GenerateKey(List<User> users)
-		{
-			string key = "";
-
-			return key;
+		public string Password
+		{ 
+			get
+			{
+				return this.password;
+			}
 		}
 	}
 }
